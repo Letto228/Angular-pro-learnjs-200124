45acc6027b678618b64a4e497357ec9579ca18a8
Letto228 <learnjs@MacBook-Pro-Egor.local>
2024-01-20 18:03:47 +0300

    feat: Pipe

--- 03-pipes/my-async/src/app/my-async.pipe.ts
+++ 03-pipes/my-async/src/app/my-async.pipe.ts
@@ -1,14 +1,39 @@
-import { Pipe, PipeTransform } from '@angular/core';
-import { Observable } from 'rxjs';
+import { ChangeDetectorRef, OnDestroy, Pipe, PipeTransform, inject } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
 
 @Pipe({
   name: 'myAsync',
   pure: false,
 })
-export class MyAsyncPipe<T> implements PipeTransform {
+export class MyAsyncPipe<T> implements PipeTransform, OnDestroy {
+  private readonly changeDetectorRef = inject(ChangeDetectorRef);
+
+  private value: T | null = null;
+  private subscription: Subscription | null = null;
+  private currentStream$: Observable<T> | null = null;
+
+  ngOnDestroy() {
+    this.subscription?.unsubscribe();
+  }
 
   transform(stream$: Observable<T>): T | null {
-    return null;
+    if (stream$ !== this.currentStream$) {
+      this.currentStream$ = stream$;
+
+      this.subscription?.unsubscribe();
+      this.subscription = null
+
+      this.value = null;
+    }
+
+    if (!this.subscription) {
+      this.subscription = stream$.subscribe(value => {
+        this.value = value;
+        this.changeDetectorRef.markForCheck();
+      });
+    }
+
+    return this.value;
   }
 
 }
