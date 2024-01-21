6fc05287cd6086e4ffa9544d4ab1f4f778f21b1f
Letto228 <learnjs@MacBook-Pro-Egor.local>
2024-01-21 08:01:33 +0300

    feat: DI

--- 05-dependency-injection/01-stand/src/app/app.component.ts
+++ 05-dependency-injection/01-stand/src/app/app.component.ts
@@ -1,10 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, InjectionToken, Injector, inject } from '@angular/core';
+import { TestService } from './test.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'stand';
+
+  constructor() {
+    // const parentToken = new InjectionToken<{name: string}>('Is library token');
+    // const token = new InjectionToken<string>('Is library token');
+
+    // const libraryInjector = Injector.create({
+    //   providers: [
+    //     {
+    //       provide: parentToken, // token
+    //       useValue: {name: 'Egor'},
+    //     },
+    //   ],
+    // });
+
+    // const injector = Injector.create({
+    //   parent: libraryInjector,
+    //   providers: [
+    //     {
+    //       provide: token, // token
+    //       useValue: 'Alex'
+    //     }
+    //   ],
+    // });
+
+    // console.log(injector.get(token), injector.get(parentToken));
+
+    const token = new InjectionToken<string>('Is library token');
+    // const token = new InjectionToken<string>('Is library token', {providedIn: 'root', factory: () => inject()});
+
+    const injector = Injector.create({
+      providers: [
+        {
+          provide: token, // token
+          useValue: 'Alex'
+        },
+        // {
+        //   provide: TestService,
+        //   useClass: TestService,
+        // }
+        TestService,
+        {
+          provide: 'name',
+          useExisting: TestService,
+        },
+        {
+          provide: 'factory',
+          useFactory: () => inject(TestService),
+          // useFactory: () => new TestService(),
+          // useFactory: () => 'Alex'
+        },
+      ],
+    });
+
+    console.log(injector.get(token), injector.get(TestService) === injector.get('name'));
+  }
 }
