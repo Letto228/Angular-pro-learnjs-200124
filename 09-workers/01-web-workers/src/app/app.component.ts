c4df27868c722b6b6725318add0f529f0d9c2d01
Letto228 <learnjs@MacBook-Pro-Egor.local>
2024-01-21 08:02:27 +0300

    feat: workers

--- 09-workers/01-web-workers/src/app/app.component.ts
+++ 09-workers/01-web-workers/src/app/app.component.ts
@@ -17,6 +17,18 @@ export class AppComponent {
   }
 
   setBigPrime() {
-    this.bigPrime = findBigPrime();
+    const worker = new Worker(
+      new URL('find-big-prime.worker', import.meta.url),
+    );
+
+    worker.addEventListener('message', ({data}) => {
+      this.bigPrime = data;
+      
+      worker.terminate();
+    });
+
+    worker.postMessage('hello');
+
+    // this.bigPrime = findBigPrime();
   }
 }
--- 09-workers/02-service-worker/src/app/app.component.ts
+++ 09-workers/02-service-worker/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { SwPush, SwUpdate, VersionReadyEvent } from '@angular/service-worker';
+import { filter, interval, timer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'service-worker';
+
+  constructor(swUpdate: SwUpdate, swPush: SwPush) {
+    Notification.requestPermission().then(permissionResult => {
+      if (permissionResult === 'granted') {
+        console.log('Can show notification');
+      }
+    })
+
+    swPush.messages.subscribe(console.log);
+
+    // interval(1000 * 60 * 2).subscribe(() => {
+    //   swUpdate.checkForUpdate().then(console.log)
+    // })
+
+    swUpdate.versionUpdates
+      .pipe(filter((event): event is VersionReadyEvent => event.type === 'VERSION_READY'))
+      .subscribe((event) => {
+        if (confirm('Есть новая версия приложения, загрузить?')) {
+          document.location.reload();
+        }
+      })
+
+    const serverPublicKey = '';
+
+    swPush.requestSubscription({serverPublicKey}).then(pushSub => {
+      
+    })
+  }
 }
+
+
